a1536a92ee9110bad888d64a8eed74c8079f69e2
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-13 16:42:01 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Cabecalho from './componets/Cabecalho/Cabecalho';
 
 function App() {
   return (
     <div className="App">
       <Cabecalho />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao react-movie
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
