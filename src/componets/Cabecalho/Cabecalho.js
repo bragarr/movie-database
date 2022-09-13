406a38146b79f53b481baab134469d0aea5140f4
filipe <filipe.dias09@hotmail.com>
2022-09-13 16:38:37 -0300

    feat logo cabeçalho

--- src/componets/Cabecalho/Cabecalho.js
+++ src/componets/Cabecalho/Cabecalho.js
@@ -3,7 +3,7 @@ import './Cabecalho.css'
 function Cabecalho (){
     return (
         <header className="cabecalho">
-            <img className="imagem" src="/img/play.png" alt="logo"/>
+            <img className="imagem" src="/img/movie-time.png" alt="logo"/>
             <h1 className="titulo__principal">MovieFlix</h1>
         </header>
     )
