6a851ad740fb85064a40055fcee86cfa0d50d2a9
filipe <filipe.dias09@hotmail.com>
2022-09-13 20:00:54 -0300

    feat titulo

--- src/componets/Cabecalho/Cabecalho.js
+++ src/componets/Cabecalho/Cabecalho.js
@@ -4,7 +4,7 @@ function Cabecalho (){
     return (
         <header className="cabecalho">
             <img className="imagem" src="/img/movie-time.png" alt="logo"/>
-            <h1 className="titulo__principal">MovieFlix</h1>
+            <h1>className="titulo__principal"MovieFlix</h1>
         </header>
     )
 }
