dee3e75c682b739dd36071df2f3561b6bff55ae9
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-13 18:54:05 -0400

    feat new folders and files

---
+++ src/components/config.js
@@ -0,0 +1,32 @@
+// Configuração para Key API
+
+
+//Consumo de API para Banco de dados de filmes e atores
+const API_URL = "https://api.themoviedb.org/3/movie/550?api_key=";
+const API_KEY = "569ff9eec8d764584edc5a6ab4f72371";
+const API_LANG = "&language=pt-BR";
+
+//Consumo de API para imagens de filmes e atores
+
+const IMAGEM_URL = "https://image.tmdb.org/t/p/";
+
+
+// Tamanho das imagens para página
+
+//para poster principal
+
+const BACKDROPE_SIZE = "w1280";
+
+// Para postes menores
+
+const POSTER_SIZE = "w500";
+
+
+export {
+    API_URL,
+    API_KEY,
+    API_LANG,
+    IMAGEM_URL,
+    BACKDROPE_SIZE,
+    POSTER_SIZE
+}
