bced0b11812ff5c936061f4af2e32428a7918303
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-14 17:01:21 -0400

    update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,19 +35,8 @@ const Home = () => {
     }, [])
 
     return (
-<<<<<<< HEAD
-        <>
-        <div>Home</div>
-        <SearchBar />
-        </>
+        <div>
+            <h1>Home</h1>
+            <SearchBar />
+        </div>
 )}
-=======
-    <div>
-        <h2>Home</h2>
-        <p>Aqui ficara todos os principais filmes</p>
-    </div>
-    )
-}
-
-export default Home;
->>>>>>> 0a5a16103175aa41cae26687c62fd86b304834f8
