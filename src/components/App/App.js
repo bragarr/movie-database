410ece446ce7d33369999c1b271f70c5e096165e
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-14 13:55:31 -0400

    feat links app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Cabecalho from '../elements/Cabecalho/Cabecalho';
 
 function App() {
@@ -5,6 +6,13 @@ function App() {
     <div className="App">
       <Cabecalho />
     </div>
+    <nav>
+      <h2>
+        <Link to="/">React Movie</Link>
+      </h2>
+      <Link to="/movie/1">Movie</Link>
+      <Link to="/search">Search</Link>
+    </nav>
   );
 }
 
