c89faf1bc7c6bed3b1dae7763f1f9a34cabdf90d
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-14 15:24:07 -0400

    feat home.js scripts

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,19 @@
-import SearchBar from "../elements/SearchBar/SearchBar"
+import React from "react";
+import "./Home.css" 
+import SearchBar from "../elements/SearchBar/SearchBar";
+
+import {
+    API_URL,
+    API_KEY,
+    API_LANG,
+    IMAGEM_URL,
+    BACKDROPE_SIZE,
+    POSTER_SIZE,
+} from "../config";
+
+import HeroImage from "../elements/HeroImage/HeroImage";
+import SearchBar from "../elements/SearchBar/SearchBar";
+
 
 const Home = () => {
     return (
