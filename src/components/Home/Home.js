f25ef08f9b39a3832927e84a3690606742deda09
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-14 16:09:10 -0400

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,27 @@
 import React from "react";
-import "./Home.css" 
-import SearchBar from "../elements/SearchBar/SearchBar";
+// import { useState, useEffect } from "react";
+import "./Home.css"; 
 
 import {
     API_URL,
     API_KEY,
-    API_LANG,
-    IMAGEM_URL,
-    BACKDROPE_SIZE,
-    POSTER_SIZE,
+    // API_LANG,
+    // IMAGEM_URL,
+    // BACKDROPE_SIZE,
+    // POSTER_SIZE,
 } from "../config";
 
-import HeroImage from "../elements/HeroImage/HeroImage";
-import SearchBar from "../elements/SearchBar/SearchBar";
+// import HeroImage from "../elements/HeroImage/HeroImage";
+// import SearchBar from "../elements/SearchBar/SearchBar";
 
+const movieURL = API_URL;
+const apiKey = API_KEY;
 
 const Home = () => {
-    return (
-        <>
-        <div>Home</div>
-        <SearchBar />
-        </>
-    )
+    console.log(movieURL);
+    console.log(apiKey);
+    return <div>Home</div>
+    
 }
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import App from './components/App/App';
+import Home from './components/Home/Home';
+import Movie from './components/Movie/Movie';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/" element={<home />} />
-          <Route path="/" element={<movie />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Movie />} />
           <Route path="/" element={<search />} />
         </Route>
       </Routes>
