41646dc9b4d1a7bac93d5690ca700f3b1fe244d1
filipe <filipe.dias09@hotmail.com>
2022-09-14 13:43:15 -0300

    organizando as tags do cabeçalho

--- src/components/elements/Cabecalho/Cabecalho.js
+++ src/components/elements/Cabecalho/Cabecalho.js
@@ -6,9 +6,15 @@ function Cabecalho (){
             <img className="imagem" src="/img/reactMovie_logo.png" alt="logo"/>
 
             <ul className="menu">
-                <li><a className="menu__movies">movies</a></li>
-                <li><img className="logo__github" src="img/github.png"/></li>
-                <li><button className="botao">login</button></li>
+                <li>
+                    <a className="menu__movies">movies</a>
+                </li>
+                <li>
+                    <img className="logo__github" src="img/github.png"/>
+                </li>
+                <li>
+                    <button type='submit' className="botao">login</button>
+                </li>
             </ul>
         </header>
     )
