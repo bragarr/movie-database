62db9e0e92a7f05e6972f3fa1f63638b56f61f31
filipe <filipe.dias09@hotmail.com>
2022-09-14 17:53:03 -0300

    estilizando a barra de busca

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,4 +6,4 @@ const Home = () => {
         <div>Home</div>
         <SearchBar />
         </>
-}
+)}
--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -3,7 +3,7 @@ import './SearchBar.css'
 function SearchBar (){
     return ( 
         <form className="idBusca">
-            <input type="text" className="textBusca" placeholder="Buscar..."/>
+            <input type="text" className="textBusca" placeholder="Buscar..." />
             <button className="btnBusca" type='submit'>Buscar</button>
         </form>
             )
