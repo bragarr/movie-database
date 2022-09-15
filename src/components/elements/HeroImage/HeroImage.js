8f90cab2cdc16e183607d9c9f6e9c4b605ac7966
filipe <filipe.dias09@hotmail.com>
2022-09-14 23:11:59 -0300

    imagem principal

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -14,9 +14,10 @@ const HeroImage = (movie) => {
         className="hero__background"
         style={{background: `url${imagemUrl}${BACKDROPE_SIZE}${movie.backdrop_path}`}}
 
-    >
-        <h2 className="hero__title">Título do filme</h2>
-        <p className="hero__description">Descrição do Filme</p>
+    >   
+        <img className="img__principal" src="./img/page.jpg"/>
+        <h1 className="hero__titulo">Fall</h1>
+        <p className="hero__description">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
     </div>
 
     )
