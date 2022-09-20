8f5c029b651675dd1b7cd4a5c95b56c4cee9d251
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-20 14:59:52 -0400

    updated files

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Outlet } from "react-router-dom";
+import Cabecalho from '../elements/Cabecalho/Cabecalho';
+
+const App = () => {
+  return (
+      <div className="App">
+        <header>
+          <Cabecalho />
+        </header>
+        <Outlet /> 
+      </div>
+  );
+}
+
+export default App;
