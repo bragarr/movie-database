b63d56bff995ebca7d352f886feb3bc83c11adf3
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-03-09 23:46:23 -0400

    feat: new dependecies - bootstrap

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,17 @@
-import "./Login.css";
-
 export function Login() {
     return (
-        <section>
+        <section className="m-5">
             <form>
-                <fieldset className="container__login">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" autoComplete="username" placeholder="Digite seu email" />
-                    <label htmlFor="passsword">Senha</label>
-                    <input type="password" name="passward" autoComplete="current-password" placeholder="Digite a sua senha"/>
-                </fieldset>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </section>
     );
